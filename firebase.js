fe78c668149c3f706fdedcc6bab662a372f9bc20
itsPritam <iampritamdas04@gmail.com>
2025-09-15 23:59:29 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,12 @@
 //  Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDxlr-FHi5ehacuomCXJ5ojOCd4yD-8rb8",
-  authDomain: "krishak-sathi-941e1.firebaseapp.com",
-  projectId: "krishak-sathi-941e1",
-  storageBucket: "krishak-sathi-941e1.firebasestorage.app",
-  messagingSenderId: "910668154081",
-  appId: "1:910668154081:web:5438724ed8e06f99d463a0",
-  measurementId: "G-YF5B3DL09E"
+  apiKey: "xxxx",
+  authDomain: "xxxx",
+  projectId: "xxxx",
+  storageBucket: "xxxx",
+  messagingSenderId: "xxxx",
+  appId: "xxxx",
+  measurementId: "xxxx"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
@@ -14,3 +14,4 @@ firebase.initializeApp(firebaseConfig);
 // Authenticate & Firestore available globally
 const auth = firebase.auth();
 const db = firebase.firestore();
+
